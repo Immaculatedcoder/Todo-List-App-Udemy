b4a3cd6b33b7832f5df99fe77a069d833eeac9ab
Adebayo Emmanuel <adebayoemmanu1111@gmail.com>
2025-06-12 22:20:23 -0400

    Update bonus8.py

--- Day8/bonus8.py
+++ Day8/bonus8.py
@@ -7,5 +7,5 @@ mood = input("How do you rate your mood from 1 to 10? ")
 thoughts = input("Let your thoughts flow: \n")
 
 with open(f"../Day8/journal/{date}", 'w') as file:
-    file.write(f"{mood}\n")
+    file.write(mood+ 2*"\n")
     file.write(thoughts)
